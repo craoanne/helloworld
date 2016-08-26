84240518bce90e8559b988125d2428e49b2b3740
Chandra Rao Anne <craoanne@wiley.com>
2016-08-26 14:33:21 -0400

    Deleted one line.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld{
    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Clone!" );
         System.out.println( "Hello Clone Second!" );
         System.out.println( "Hello Clone Third!" );
         
